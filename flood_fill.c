209c77652f1d30bde22beab829143e74586218ac
Axel DHONDT <adhondt@e1r12p21.42.fr>
2018-06-18 17:05:46 +0200

    auto commit

---
+++ flood_fill.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   flood_fill.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adhondt <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/18 17:04:13 by adhondt           #+#    #+#             */
+/*   Updated: 2018/06/18 17:05:04 by adhondt          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include "t_point.h"
+
+void	ft_puttab(char **tab)
+{
+	int i;
+	int	j;
+
+	i = 0;
+	while (tab[i])
+	{
+		j = -1;
+		while (tab[i][++j])
+			write(1, &tab[i][j], 1);
+		write(1, "\n", 1);
+		i++;
+	}
+}
+
+void	fill(char **tab, int x, int y, char f, int ymax)
+{
+	if (x < 0 || y < 0)
+		return ;
+	if (tab[y][x] == f)
+		tab[y][x] = 'F';
+	if (tab[y][x + 1] == f)
+		fill(tab, x + 1, y, f, ymax);
+	if (x - 1 >= 0 && tab[y][x - 1] == f)
+		fill(tab, x - 1, y, f, ymax);
+	if (y + 1 != ymax && tab[y + 1][x] == f)
+		fill(tab, x, y + 1, f, ymax);
+	if (y - 1 >= 0 && tab[y - 1][x] == f)
+		fill(tab, x, y - 1, f, ymax);
+}
+
+void	flood_fill(char **tab, t_point size, t_point begin)
+{
+	int	x;
+	int	y;
+	char f;
+
+	if (begin.y < 1 || begin.x < 1)
+		return ;
+	x = begin.x - 1;
+	y = begin.y - 1;
+	f = tab[y][x];
+	fill(tab, x, y, f, size.y);
+	return ;
+}
+
+char	**make_area(char **zone, int x, int y)
+{
+	int	i;
+	int j;
+	char	**area;
+
+	area = (char **)malloc(sizeof(char *) * (y + 1));
+	area[y] = NULL;
+	i = -1;
+	while (++i < y)
+	{
+		j = -1;
+		area[i] = (char *)malloc(sizeof(char) * (x + 1));
+		area[i][x] = '\0';
+		while(zone[i][++j])
+			area[i][j] = zone[i][j];
+	}
+	return (area);
+}
+
+int main(void)
+{
+	char **area;
+	t_point size = { 8, 5 };
+	t_point begin = { 7, 5 };
+	char *zone[] = {
+		"11111111",
+		"10001001",
+		"10010001",
+		"10110001",
+		"11100001"
+	};
+
+	area = make_area(zone, size.x, size.y);
+	ft_puttab(area);
+	flood_fill(area, size, begin);
+	write(1, "\n", 1);
+	ft_puttab(area);
+	return (0);
+}
